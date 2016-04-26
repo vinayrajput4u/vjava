1dad03c04b974a1e4069dc23da43593ddc83de74
Vinaysingh Rajput <vinayrajput4u@gmail.com>
2016-04-26 16:57:19 +0530

    Initial Commit

---
+++ FirstClass.java
@@ -0,0 +1,11 @@
+public class FirstClass
+{
+	public static void main (String [] args)
+	{
+			System.out.println("My First Program From Java");
+			System.out.println("Second Print Command.");
+		
+		
+	}
+	
+}
